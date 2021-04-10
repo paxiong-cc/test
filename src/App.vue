22faba0be95f1f5c8f224fb17bc006b2fd380b5d
Administrator <576303283@qq.com>
2021-04-10 10:26:05 +0800

    添加备案信息

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
-  <div>
-    <div>999</div>
-    <Header @adds="add" />
-    <div v-for="(item, idx) in list" :key="idx" style="display: flex">
-      <div style="margin-right: 10px">{{ item }}</div>
-      <input v-model="list[idx]" type="text" style="margin-right: 10px" />
-      <div @click="deletes(idx)" class="delete" style="margin-right: 10px">-</div>
-      <div @click="edit(idx)">edit</div>
+  <div class="cc">
+    <div>
+      <div>999</div>
+      <Header @adds="add" />
+      <div v-for="(item, idx) in list" :key="idx" style="display: flex">
+        <div style="margin-right: 10px">{{ item }}</div>
+        <input v-model="list[idx]" type="text" style="margin-right: 10px" />
+        <div @click="deletes(idx)" class="delete" style="margin-right: 10px">-</div>
+        <div @click="edit(idx)">edit</div>
+      </div>
+      <AsdHeader></AsdHeader>
     </div>
-    <AsdHeader></AsdHeader>
+
+    <div class="footer">
+      <a href="https://beian.miit.gov.cn/#/Integrated/index" class="record" target="_blank">
+        蜀ICP备2021006541号
+      </a>
+    </div>
   </div>
 </template>
 
@@ -42,5 +50,36 @@ export default {
 </script>
 
 <style>
+html, body {
+  height: 100%;
+}
 
+* {
+  margin: 0;
+  padding: 0;
+}
+
+.cc {
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+}
+
+.footer {
+  height: 100px;
+  text-align: center;
+  line-height: 100px;
+  background-color: black;
+}
+.record {
+  margin-bottom: 20px;
+  color: #bbb;
+  cursor: pointer;
+  text-decoration: none;
+}
+.record:hover {
+  color: #ccc
+}
 </style>
