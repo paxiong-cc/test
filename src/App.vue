ca29315fd2e97e181089498202322cf655bc3602
paxiong-cc <40492797+paxiong-cc@users.noreply.github.com>
2022-06-17 17:05:27 +0800

    xxx

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <div>paxiongdiv>
       <Header @adds="add" />
       <div v-for="(item, idx) in list" :key="idx" style="display: flex">
-        <div style="margin-right: 10px">{{ item }}</div>
+        <div style="margin-right: 10px">{{ item }}1</div>
         <input v-model="list[idx]" type="text" style="margin-right: 10px" />
         <div @click="deletes(idx)" class="delete" style="margin-right: 10px">-</div>
         <div @click="edit(idx)">edit</div>
